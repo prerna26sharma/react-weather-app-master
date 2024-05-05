8dc0f97a6c8f78d7067a4c191396519c289d8948
Prerna Sharma <130654046+prerna26sharma@users.noreply.github.com>
2024-05-05 15:42:21 +0530

    Update WeatherInfoComponent.js

--- src/modules/WeatherInfoComponent.js
+++ src/modules/WeatherInfoComponent.js
@@ -2,6 +2,13 @@ import React from "react";
 import styled from "styled-components";
 import {WeatherIcons} from "../App";
 
+export const WeatherInfoIcons = {
+    sunset: "/react-weather-app/icons/temp.svg",
+    sunrise: "/react-weather-app/icons/temp.svg",
+    humidity: "/react-weather-app/icons/humidity.svg",
+    wind: "/react-weather-app/icons/wind.svg",
+    pressure: "/react-weather-app/icons/pressure.svg",
+};
 const Location = styled.span`
   margin: 15px auto;
   text-transform: capitalize;
@@ -16,7 +23,14 @@ const Condition = styled.span`
     font-size: 28px;
   }
 `;
-
+const WeatherInfoLabel = styled.span`
+  margin: 20px 25px 10px;
+  text-transform: capitalize;
+  text-align: start;
+  width: 90%;
+  font-weight: bold;
+  font-size: 14px;
+`;
 const WeatherIcon = styled.img`
   width: 100px;
   height: 100px;
@@ -30,9 +44,54 @@ const WeatherContainer = styled.div`
   justify-content: space-between;
   align-items: center;
 `;
+const WeatherInfoContainer = styled.div`
+  display: flex;
+  width: 90%;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-wrap: wrap;
+`;
+const InfoContainer = styled.div`
+  display: flex;
+  margin: 5px 10px;
+  flex-direction: row;
+  justify-content: space-evenly;
+  align-items: center;
+`;
+const InfoIcon = styled.img`
+  width: 36px;
+  height: 36px;
+`;
+const InfoLabel = styled.span`
+  display: flex;
+  flex-direction: column;
+  font-size: 14px;
+  margin: 15px;
+  & span {
+    font-size: 12px;
+    text-transform: capitalize;
+  }
+`;
 
+const WeatherInfoComponent = (props) => {
+    const {name, value} = props;
+    return (
+        <InfoContainer>
+            <InfoIcon src={WeatherInfoIcons[name]}/>
+            <InfoLabel>
+                {value}
+                <span>{name}</span>
+            </InfoLabel>
+        </InfoContainer>
+    );
+};
 const WeatherComponent = (props) => {
     const {weather} = props;
+    const isDay = weather?.weather[0].icon?.includes('d')
+    const getTime = (timeStamp) => {
+        return `${new Date(timeStamp * 1000).getHours()} : ${new Date(timeStamp * 1000).getMinutes()}`
+    }
     return (
         <>
             <WeatherContainer>
@@ -43,7 +102,15 @@ const WeatherComponent = (props) => {
                 <WeatherIcon src={WeatherIcons[weather?.weather[0].icon]}/>
             </WeatherContainer>
             <Location>{`${weather?.name}, ${weather?.sys?.country}`}</Location>
-            
+
+            <WeatherInfoLabel>Weather Info</WeatherInfoLabel>
+            <WeatherInfoContainer>
+                <WeatherInfoComponent name={isDay ? "sunset" : "sunrise"}
+                                      value={`${getTime(weather?.sys[isDay ? "sunset" : "sunrise"])}`}/>
+                <WeatherInfoComponent name={"humidity"} value={weather?.main?.humidity}/>
+                <WeatherInfoComponent name={"wind"} value={weather?.wind?.speed}/>
+                <WeatherInfoComponent name={"pressure"} value={weather?.main?.pressure}/>
+            </WeatherInfoContainer>
         </>
     );
 };
